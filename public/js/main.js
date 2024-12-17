a138cc148c80e58b3ce84195f8a8197c4d678a5d
Kelwilson <kelwilson_achienu@icloud.com>
2024-12-16 22:21:47 -0800

    Added a markcomplete and mark incomplete function on the client side

--- public/js/main.js
+++ public/js/main.js
@@ -5,11 +5,11 @@ const editButton = document.querySelectorAll('.fa-pen-to-square')
 const trashButton = document.querySelectorAll('.fa-trash-can')
 
 
-Array.from(taskComplete).forEach(btn => btn.addEventListener('click', markComplete))
+Array.from(taskComplete).forEach(btn => btn.addEventListener('click', markIncomplete))
 
-// Array.from(taskItem).forEach(btn => btn.addEventListener('click', markComplete))
+Array.from(taskItem).forEach(btn => btn.addEventListener('click', markComplete))
 
-Array.from(checkButton).forEach(btn => btn.addEventListener('click', markComplete))
+Array.from(checkButton).forEach(btn => btn.addEventListener('change', toggleCheckbox))
 
 // Array.from(editButton).forEach(btn => btn.addEventListener('click', editTask))
 
@@ -17,11 +17,30 @@ Array.from(checkButton).forEach(btn => btn.addEventListener('click', markComplet
 
 // Array.from(checkButton).forEach(btn => btn.addEventListener('click', markInComplete))
 
+//checking to see if task are marked complete and changing the attribute of the checkbox accordingly
+
+
+
+// Best part of my code where i check and updated the checkbox and completed task values
+// checking if checked is true on the checkbox and updating it accordingly
+function toggleCheckbox(event) {
+    console.log('wow')
+   const btn = event.target
+if(btn.checked) {
+    console.log('lets go')
+    markComplete()
+} else {
+    console.log('not again')
+    markIncomplete()
+}
+}
+
+
 
 // mark complete button
 async function markComplete() {
-    console.log
-    const taskId = this.parentNode.dataset.id
+    // const taskId = this.parentNode?.dataset.id
+     const taskId = event.currentTarget.closest('.taskItem')?.dataset.id;
     console.log('yeah we outside', taskId)
     try{
         const response = await fetch('tasks/markComplete', {
@@ -38,4 +57,25 @@ async function markComplete() {
         console.log(err)
     }
 
+}
+
+async function markIncomplete(){
+    // const taskId = this.parentNode?.dataset.id
+    const taskId = event.currentTarget.closest('.taskItem')?.dataset.id;
+    console.log('yeah we back', taskId)
+    
+    try{
+        const response = await fetch('tasks/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'taskIdFromJSFile': taskId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
 }
