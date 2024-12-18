43d7256afb1582e741759aa00e84fa9bfb1327d3
Kelwilson <kelwilson_achienu@icloud.com>
2024-12-18 11:54:30 -0800

    Added a method override middle ware

---
+++ middleware/method.js
@@ -0,0 +1,10 @@
+module.exports = {
+    override: (req, res) => {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+          // look in urlencoded POST bodies and delete it
+          var method = req.body._method
+          delete req.body._method
+          return method
+        }
+}
+}
