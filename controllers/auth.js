c6c717dc07b26ec6289a205f430443f6deac8a56
Kelwilson <kelwilson_achienu@icloud.com>
2024-12-27 12:14:35 -0800

    Updated the auth.js code

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,41 +56,100 @@ const User = require('../models/User')
     })
   }
   
-  exports.postSignup = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
+  // Old deprecated code 
+  // exports.postSignup = (req, res, next) => {
+  //   const validationErrors = []
+  //   if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+  //   if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
+  //   if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
   
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('../signup')
+  //   if (validationErrors.length) {
+  //     req.flash('errors', validationErrors)
+  //     return res.redirect('../signup')
+  //   }
+  //   req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+  
+  //   const user = new User({
+  //     userName: req.body.userName,
+  //     email: req.body.email,
+  //     password: req.body.password
+  //   })
+  
+  //   User.findOne({$or: [
+  //     {email: req.body.email},
+  //     {userName: req.body.userName}
+  //   ]}, (err, existingUser) => {
+  //     if (err) { return next(err) }
+  //     if (existingUser) {
+  //       req.flash('errors', { msg: 'Account with that email address or username already exists.' })
+  //       return res.redirect('../signup')
+  //     }
+  //     user.save((err) => {
+  //       if (err) { return next(err) }
+  //       req.logIn(user, (err) => {
+  //         if (err) {
+  //           return next(err)
+  //         }
+  //         res.redirect('/tasks')
+  //       })
+  //     })
+  //   })
+  // }
+
+
+  // New code
+  exports.postSignup = async (req, res, next) => {
+    const validationErrors = [];
+  
+    // Validate user input
+    if (!validator.isEmail(req.body.email)) {
+      validationErrors.push({ msg: 'Please enter a valid email address.' });
     }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+    if (!validator.isLength(req.body.password, { min: 8 })) {
+      validationErrors.push({ msg: 'Password must be at least 8 characters long.' });
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+      validationErrors.push({ msg: 'Passwords do not match.' });
+    }
   
+    // If there are validation errors, redirect back with error messages
+    if (validationErrors.length) {
+      req.flash('errors', validationErrors);
+      return res.redirect('../signup');
+    }
+  
+    // Normalize email
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
+  
+    // Create a new user object
     const user = new User({
       userName: req.body.userName,
       email: req.body.email,
-      password: req.body.password
-    })
+      password: req.body.password,
+    });
   
-    User.findOne({$or: [
-      {email: req.body.email},
-      {userName: req.body.userName}
-    ]}, (err, existingUser) => {
-      if (err) { return next(err) }
+    try {
+      // Check if a user with the same email or username already exists
+      const existingUser = await User.findOne({
+        $or: [{ email: req.body.email }, { userName: req.body.userName }],
+      });
+  
       if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address or username already exists.' })
-        return res.redirect('../signup')
+        req.flash('errors', { msg: 'Account with that email address or username already exists.' });
+        return res.redirect('../signup');
       }
-      user.save((err) => {
-        if (err) { return next(err) }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err)
-          }
-          res.redirect('/tasks')
-        })
-      })
-    })
-  }
+  
+      // Save the new user
+      await user.save();
+  
+      // Log in the user after successful signup
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/tasks');
+      });
+    } catch (err) {
+      next(err); // Pass errors to the error-handling middleware
+    }
+  };
