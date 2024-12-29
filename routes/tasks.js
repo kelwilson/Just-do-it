cf2a7ed79615f73aaee5619d7ce87f05bfcc2cfe
Kelwilson <kelwilson_achienu@icloud.com>
2024-12-28 23:46:15 -0800

    Added ensure auth to the task root path

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const taskController = require('../controllers/tasks')
+const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', taskController.getTasks)
+router.get('/', ensureAuth, taskController.getTasks)
+router.get('/:id', taskController.getTaskId)
 router.post('/createTask', taskController.createTask)
 router.put('/markComplete', taskController.markComplete)
 router.put('/markIncomplete', taskController.markIncomplete)
 router.delete('/deleteTask', taskController.deleteTask)
-
+router.put('/:id/update', taskController.updateTask)
 
 
 
