82c801e10aeb55ccac052f8429c2d06da7cc5e5c
Kelwilson <kelwilson_achienu@icloud.com>
2025-01-01 21:36:09 -0800

    Added a method override middleware

--- middleware/method.js
+++ middleware/method.js
@@ -1,9 +1,12 @@
+const { put } = require("../routes/main")
+
 module.exports = {
     override: (req, res) => {
         if (req.body && typeof req.body === 'object' && '_method' in req.body) {
           // look in urlencoded POST bodies and delete it
           var method = req.body._method
-          delete req.body._method
+          // delete req.body._method
+          put req.body._method
           return method
         }
 }
